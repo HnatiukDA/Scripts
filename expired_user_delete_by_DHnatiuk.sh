e2bf26fdd2bb525ffdf2d263abd98eb6a0dacb14
Dmytro Hnatiuk <gnatyuk.d.a.95@gmail.com>
2023-05-09 16:10:40 +0300

    init

--- expired_user_delete_by_DHnatiuk.sh
+++ expired_user_delete_by_DHnatiuk.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Check for root
-if [[ "${UID}" -ne 0 ]]
-then
-    echo "You should run this script as a root!"
-    exit 1
-fi
-
 # Set the number of days that user hasn't login
 DAYS=30
 
@@ -16,9 +9,6 @@ CURRENT_DATE=$(date +%s)
 for username in $(cut -d ':' -f1 /etc/passwd);
 do
     LAST_LOGIN=$(lastlog -u $username | awk 'NR==2{print $4, $5, $6}')
-    if [[ $LAST_LOGIN == "**Never**" ]]; then
-        continue
-    fi
     LAST_LOGIN_TIMESTAMP=$(date -d "$LAST_LOGIN" +%s)
 
     # Days since user last login
